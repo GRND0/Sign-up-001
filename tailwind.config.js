4521cec5d4cf7f2076933b798987481d76589db1
GRND0 <zefiro.turchetta@gmail.com>
2023-03-14 15:02:27 +0100

    aggiunte regole custom Tailwind, inizio sviluppo interfaccia

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "z-rosa": "#d97eb0",
+        "z-arancio": "#f2ca80",
+        "z-testo-enfasi": "#5f5e5e",
+        "z-testo": "#7d7c7c",
+      },
+      backgroundImage: {
+        "z-shake": "../assets/shake.jpg",
+        "z-utente" : "../assets/User.png"
+      },
+
+      fontFamily: {
+        "z-titolo": "Aladin, cursive",
+        "z-testi": "Nunito, sans-serif",
+      },
+    },
   },
   plugins: [],
-}
+};
